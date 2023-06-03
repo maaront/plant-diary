1edce4b29fe08086e2ba5aff7c5624445c9dcc0f
Claudia Gillota <cgillota@gmail.com>
2023-06-03 16:41:42 -0400

    fixed and added helpers

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -3,11 +3,31 @@ const { response } = require("express");
 
 router.get('/', async (req, res) => {
     try {
-        res.render('homepage');
+        res.render('homepage', {  logged_in: req.session.logged_in });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
     }
 });
+
+router.get('/login',  (req, res) => {
+   if (req.session.logged_in) {
+    res.redirect('/dashboard');
+   }
+
+       res.render('login');
+   
+   
+});
+
+router.get('/signup',  (req, res) => {
+    if (req.session.logged_in) {
+     res.redirect('/dashboard');
+    }
+ 
+        res.render('createaccount');
+    
+    
+ });
 
 module.exports = router;
