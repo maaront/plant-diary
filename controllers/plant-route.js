947f6a06198d430cb9ef8eb3ae74a4aa89957029
maaront <35611834+maaront@users.noreply.github.com>
2023-06-03 17:35:31 -0400

    get individual plants to link

--- controllers/plant-route.js
+++ controllers/plant-route.js
@@ -3,56 +3,6 @@ const { response } = require("express");
 const { Plant } = require("../models");
 const axios = require('axios'); // import axios
 
-// GET all plants from Trefle API. This one works for a static HTML page
-// router.get('/:plantName', async (req, res) => {
-  
-//   const plantName = req.params.plantName;
-//   const token = 't_RrrFDUYpfQ6Dj_7jRMH3QPJENvdDDklPweJJNX-XU';
-
-//   try {
-//       axios.get(`https://trefle.io/api/v1/plants/search?token=${token}&q=${plantName}`)
-//         .then( response => {
-//           console.log(response.data)
-//           res.json(response.data);
-//           // res.render('plant-search', { plant: response.data });
-//         })
-      
-//   } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ message: 'Server error' });
-//   }
-// });
-
-
-
-// GET all plants from Trefle API. This one is for the handlebars page NOT WORKING
-// router.get('/:plantName', async (req, res) => {
-  
-//   const plantName = req.params.plantName;
-//   const token = 't_RrrFDUYpfQ6Dj_7jRMH3QPJENvdDDklPweJJNX-XU';
-
-//   try {
-//       axios.get(`https://trefle.io/api/v1/plants/search?token=${token}&q=${plantName}`)
-//         .then( response => {
-//           console.log(response.data)
-//           const data = {
-//             plants: plants.map(plant => {
-//               return {
-//                 common_name: plant.common_name,
-//               }
-//             })
-//           }
-//           res.render('searchplants', data);
-//         })
-      
-//   } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ message: 'Server error' });
-//   }
-// });
-
-
-
 // Render search page
 router.get('/search', async (req, res) => {
   try {
@@ -63,7 +13,7 @@ router.get('/search', async (req, res) => {
   }
 });
 
-//Take 3 to get handlebars to work
+//Post all searched for plants from API to a page
 router.post('/search', async (req, res) => {
   const plantName = req.body.plantName;
   const token = 't_RrrFDUYpfQ6Dj_7jRMH3QPJENvdDDklPweJJNX-XU';
@@ -83,31 +33,56 @@ router.post('/search', async (req, res) => {
   }
 });
 
+// Render plant page
+// router.get('/', async (req, res) => {
+//   try {
+//       res.render('plant');
+//   } catch (err) {
+//       console.error(err);
+//       res.status(500).json({ message: 'Server error' });
+//   }
+// });
 
-// GET one plant
-router.get("/plant/:id", async (req, res) => {
+// Post one plant from search to a page
+router.get('/:plantName', async (req, res) => {
+  const plantName = req.params.plantName;
+  
+  const token = 't_RrrFDUYpfQ6Dj_7jRMH3QPJENvdDDklPweJJNX-XU';
   try {
-    const plantData = await Plant.findByPk(req.params.id);
-    const plant = plantData.get({ plain: true });
-    res.render("plant", { plant, logged_in: req.session.logged_in });
+    const response = await axios.get(`https://trefle.io/api/v1/plants?token=${token}&filter[common_name]=${plantName}`);
+    const plants = response.data.data;
+    //res.json(plants[0]);
+    // const data = {
+    //   plants: plants.map(plant => ({
+    //     common_name: plant.common_name,
+    //     plant_id: plant.plant_id
+    //   }))
+    // };
+    // console.log(data);
+
+    res.render('plant', plants[0]); // Render the plant handlebars page
   } catch (err) {
-    res.status(500).json(err);
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
   }
 });
+
+
+
 
 
 // GET all plants for a user
-router.get("/", async (req, res) => {
-  try {
-    const plantData = await Plant.findAll({
-      where: { user_id: req.session.user_id },
-    });
-    const plants = plantData.map((plant) => plant.get({ plain: true }));
-    res.render("all-plants", { plants, logged_in: req.session.logged_in });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", async (req, res) => {
+//   try {
+//     const plantData = await Plant.findAll({
+//       where: { user_id: req.session.user_id },
+//     });
+//     const plants = plantData.map((plant) => plant.get({ plain: true }));
+//     res.render("all-plants", { plants, logged_in: req.session.logged_in });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 
 
@@ -132,44 +107,44 @@ router.get("/", async (req, res) => {
 // });
 
 // POST add plant
-router.post("/add-plant", async (req, res) => {
-  try {
-    const newPlant = await Plant.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-    res.status(200).json(newPlant);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.post("/add-plant", async (req, res) => {
+//   try {
+//     const newPlant = await Plant.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+//     res.status(200).json(newPlant);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 // PUT edit plant
-router.put("/plant/edit/:id", async (req, res) => {
-  try {
-    const plantData = await Plant.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(plantData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.put("/plant/edit/:id", async (req, res) => {
+//   try {
+//     const plantData = await Plant.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json(plantData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 // DELETE plant
-router.delete("/plant/:id", async (req, res) => {
-  try {
-    const plantData = await Plant.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(plantData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.delete("/plant/:id", async (req, res) => {
+//   try {
+//     const plantData = await Plant.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json(plantData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 module.exports = router;
--- public/js/plant-search.js
+++ public/js/plant-search.js
@@ -24,34 +24,24 @@ document.getElementById("search-form").addEventListener("submit", function (even
         // Clear out any old results
         resultsDiv.innerHTML = "";
   
-        // Create container for plant list
-        const plantList = document.createElement("ul");
-  
         // Loop over the data (the plants)
         response.plants.forEach((plant) => {
-          // Create a new list item for each plant
-          const plantListItem = document.createElement("li");
-  
           // Create a new anchor element for each plant
           const plantLink = document.createElement("a");
   
           // Set the text of the anchor element to the common name of the plant
           plantLink.textContent = plant.common_name;
   
-          // Set the href attribute of the anchor element to the plant's ID
-          plantLink.href = `/plants/${plant.id}`;
+          // Set the href attribute of the anchor element to navigate to the plant page
+        //   plantLink.href = `/plant?id=${plant.id}`;
+          plantLink.href = `/plant/${plant.common_name}`;
+        
   
-          // Append the anchor element to the list item element
-          plantListItem.appendChild(plantLink);
-  
-          // Append the list item element to the plant list
-          plantList.appendChild(plantListItem);
+          // Append the anchor element to the 'results' div
+          resultsDiv.appendChild(plantLink);
   
           console.log(plant);
         });
-  
-        // Append the plant list to the 'results' div
-        resultsDiv.appendChild(plantList);
       })
       .catch((error) => console.error("Error:", error));
   });
