f6bb48d6efd9bc327bf18408f27dcd0cda605151
maaront <35611834+maaront@users.noreply.github.com>
2023-06-05 16:44:29 -0400

    prevent logout.js from loading until html fully loads

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,18 @@
 const logout = async () => {
-    // Make a POST request to destroy the session on the back end
-    const response = await fetch('/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      // If successfully logged out, redirect to the homepage page
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
+  // Make a POST request to destroy the session on the back end
+  const response = await fetch('/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    // If successfully logged out, redirect to the homepage page
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+window.onload = function() {
   document.getElementById('logout').addEventListener('click', logout);
-  
+};
