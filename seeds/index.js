7e56197354c0aa72df5a4e2522c9b05c721f4bde
[Dan <123705636+dkopac@users.noreply.github.com>
2023-06-04 22:44:37 -0400

    seeds working

--- seeds/index.js
+++ seeds/index.js
@@ -1,22 +1,27 @@
-const seedPlants = require('./plant-seeds');
-const seedDiary = require('./diary-seeds');
-const seedUsers = require('./user-seeds');
+const seedPlants = require("./plant-seeds");
+const seedDiary = require("./diary-seeds");
+const seedUsers = require("./user-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
+  // Disable foreign key checks
+  await sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
+
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
- 
+  console.log("\n----- DATABASE SYNCED -----\n");
+
+  // Enable foreign key checks
+  await sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
+
   await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
+  console.log("\n----- USERS SEEDED -----\n");
 
   await seedPlants();
-  console.log('\n----- PLANTS SEEDED -----\n');
+  console.log("\n----- PLANTS SEEDED -----\n");
 
-   
   await seedDiary();
-  console.log('\n----- DIARY SEEDED -----\n');
+  console.log("\n----- DIARY SEEDED -----\n");
 
   process.exit(0);
 };
