d1b20947a6e94d3e6a79dbe97e122518a5e703b1
maaront <35611834+maaront@users.noreply.github.com>
2023-06-05 22:12:54 -0400

    return UL for APi results

--- public/js/plant-search.js
+++ public/js/plant-search.js
@@ -1,48 +1,53 @@
 // Listen for the form submit event
 document.getElementById("search-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-  
-    // Get the plant name from the form input
-    const plantName = document.getElementById("search-input").value;
-  
-    // Send a POST request to the /search route on the server
-    fetch(`/plant/search`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Convert the JavaScript object to a JSON string
-      body: JSON.stringify({ plantName: plantName })
+  event.preventDefault();
+
+  // Get the plant name from the form input
+  const plantName = document.getElementById("search-input").value;
+
+  // Send a POST request to the /search route on the server
+  fetch(`/plant/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Convert the JavaScript object to a JSON string
+    body: JSON.stringify({ plantName: plantName })
+  })
+    // Parse the response as JSON
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      // Get the 'results' div
+      const resultsDiv = document.getElementById("search-results");
+
+      // Clear out any old results
+      resultsDiv.innerHTML = "";
+
+      // Create a new unordered list
+      const resultsList = document.createElement("ul");
+
+      // Loop over the data (the plants)
+      response.plants.forEach((plant) => {
+        // Create a new list item and anchor element for each plant
+        const listItem = document.createElement("li");
+        const plantLink = document.createElement("a");
+
+        // Set the text of the anchor element to the common name of the plant
+        plantLink.textContent = plant.common_name;
+
+        // Set the href attribute of the anchor element to navigate to the plant page
+        plantLink.href = `/plant/${plant.common_name}`;
+      
+        // Append the anchor element to the list item, and append the list item to the results list
+        listItem.appendChild(plantLink);
+        resultsList.appendChild(listItem);
+
+        console.log(plant);
+      });
+
+      // Append the results list to the 'results' div
+      resultsDiv.appendChild(resultsList);
     })
-      // Parse the response as JSON
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        // Get the 'results' div
-        const resultsDiv = document.getElementById("search-results");
-  
-        // Clear out any old results
-        resultsDiv.innerHTML = "";
-  
-        // Loop over the data (the plants)
-        response.plants.forEach((plant) => {
-          // Create a new anchor element for each plant
-          const plantLink = document.createElement("a");
-  
-          // Set the text of the anchor element to the common name of the plant
-          plantLink.textContent = plant.common_name;
-  
-          // Set the href attribute of the anchor element to navigate to the plant page
-        //   plantLink.href = `/plant?id=${plant.id}`;
-          plantLink.href = `/plant/${plant.common_name}`;
-        
-  
-          // Append the anchor element to the 'results' div
-          resultsDiv.appendChild(plantLink);
-  
-          console.log(plant);
-        });
-      })
-      .catch((error) => console.error("Error:", error));
-  });
-  
+    .catch((error) => console.error("Error:", error));
+});
