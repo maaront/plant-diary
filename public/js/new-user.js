433f32bdb719a3235a7fa0458051aaeba753aed8
[Dan <123705636+dkopac@users.noreply.github.com>
2023-06-05 23:20:40 -0400

    updates to new-user

--- public/js/new-user.js
+++ public/js/new-user.js
@@ -2,5 +2,29 @@ const userFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values for new user
-  const create-account = document.querySelector("#create-account").value.trim();
-    const password = document.querySelector("#password").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch("/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // If successful, redirect the browser to the dashboard page
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  let signupForm = document.querySelector(".signup-form");
+  if (signupForm) {
+    signupForm.addEventListener("submit", userFormHandler);
+  }
+});
