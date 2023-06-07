b5c158a9d6f69a8a7086c8295ef069222e4fa19e
[Dan <123705636+dkopac@users.noreply.github.com>
2023-06-06 22:25:38 -0400

    trying to figure it out

--- public/js/new-user.js
+++ public/js/new-user.js
@@ -1,30 +1,26 @@
-const userFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  // Collect values for new user
-  const username = document.querySelector("#username-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const user_name = document.querySelector("#username-signup").value.trim();
+  const user_password = document.querySelector("#password-signup").value.trim();
 
-  if (username && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch("/users", {
+  if (user_name && user_password) {
+    const response = await fetch("/", {
       method: "POST",
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ user_name, user_password }),
       headers: { "Content-Type": "application/json" },
     });
 
-    // If successful, redirect the browser to the dashboard page
     if (response.ok) {
       document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
     }
   }
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  let signupForm = document.querySelector(".signup-form");
+  let signupForm = document.getElementById("signup-form");
+
   if (signupForm) {
-    signupForm.addEventListener("submit", userFormHandler);
+    signupForm.addEventListener("submit", signupFormHandler);
   }
 });
