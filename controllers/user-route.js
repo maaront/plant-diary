b44b03f258129c6c00d6d9cf4929e58199d58f3a
maaront <35611834+maaront@users.noreply.github.com>
2023-06-07 20:04:02 -0400

    create new post new user route

--- controllers/user-route.js
+++ controllers/user-route.js
@@ -75,101 +75,41 @@ router.get("/", async (req, res) => {
 });
 
 // POST a new user
-// router.post("/", async (req, res) => {
-//   try {
-//     const newUser = await User.create(req.body);
-//     res.status(200).json(newUser);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.post("/create-account", async (req, res) => {
+  try {
+    // Check if a user already exists with this username
+    const existingUser = await User.findOne({
+      where: { user_name: req.body.user_name },
+    });
 
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "A user with this username already exists" });
+    }
 
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(req.body.user_password, 10);
 
-// // GET a single user
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const userData = await User.findByPk(req.params.id, {
-//       include: [{ model: Plant }, { model: Diary }],
-//     });
-//     const user = userData.get({ plain: true });
-//     console.log(user);
-//     res.json(user);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    // Create a new user
+    const newUser = await User.create({
+      user_name: req.body.user_name,
+      user_password: hashedPassword,
+      // add more fields here if necessary
+    });
 
-// // POST login
-// router.post("/login", async (req, res) => {
-//   try {
-//     const userData = await User.findOne({
-//       where: { email: req.body.email },
-//     });
-//     if (!userData) {
-//       res
-//         .status(400)
-//         .json({ message: "Incorrect email or password, please try again" });
-//       return;
-//     }
-//     const validPassword = await userData.checkPassword(req.body.password);
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: "Incorrect email or password, please try again" });
-//       return;
-//     }
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
-//       res.json({ user: userData, message: "You are now logged in!" });
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+    // Save the user's id and logged_in status in session
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+    });
 
-// // POST logout
-// router.post("/logout", (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
-// // PUT a user
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const userData = await User.update(req.body, {
-//       where: { id: req.params.id },
-//     });
-//     if (!userData) {
-//       res.status(404).json({ message: "No user found with this id!" });
-//       return;
-//     }
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// // DELETE a user
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const userData = await User.destroy({
-//       where: { id: req.params.id },
-//     });
-//     if (!userData) {
-//       res.status(404).json({ message: "No user found with this id!" });
-//       return;
-//     }
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
